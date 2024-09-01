2405456789b6fb3192e24dcddfbf177be9d1bb90
Denner Silva <denners199@gmail.com>
2024-09-01 14:45:27 -0300

    Primeiro commit no Intellij

---
+++ src/denner/anatomia/MinhaClasses.java
@@ -0,0 +1,18 @@
+package edu.denner.anatomia;
+
+public class MinhaClasses {
+
+    public static void main(String[] args) {
+        String primeiroNome = "Denner";
+        String segundoNome = "Silva";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" " ).concat(segundoNome);
+    }
+
+}
